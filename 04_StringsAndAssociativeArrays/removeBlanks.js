4532a5187d7995af81f64006d59184cd5f52b14a
Jason <jason.w.switzer@gmail.com>
2018-01-19 16:51:03 -0800

    completed removeBlanks

--- 04_StringsAndAssociativeArrays/removeBlanks.js
+++ 04_StringsAndAssociativeArrays/removeBlanks.js
@@ -1,1 +1,8 @@
 //create a function that, given a string, returns all of that string's contents, but wihout blanks
+
+function removeBlanks(string) {
+	var output = string.replace(/\s/g, "");
+	return output;
+}
+
+removeBlanks("Dr  o p T h    e    B e a t");
